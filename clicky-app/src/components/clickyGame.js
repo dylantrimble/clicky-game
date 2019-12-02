9107d2703ad1629bbf0a9b434835d6cde31cb618
dyl trimble <dyltrimble12@gmail.com>
2019-12-02 14:52:28 -0500

    made clickyGame component outline, logic to come with use of props more than likely

---
+++ clicky-app/src/components/clickyGame.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Container from './Container';
+import Footer from './Footer';
+import Banner from './Banner';
+import images from '../images';
+
+class ClickyGame extends Component {
+
+    // add in logic for game functionality and make this a "smart component" in order to makes each component have a state and atribute.
+
+render() {
+    return (
+      <div>
+        <Navbar />
+        <Banner />
+        <Container />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default ClickyGame;
